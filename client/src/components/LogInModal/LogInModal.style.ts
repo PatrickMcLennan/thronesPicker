a0e331c3458a9167e0dd2c162a027d2851981c35
Patrick <patrick.a.mclennan@gmail.com>
2019-04-21 15:29:11 -0400

    most functionality + full stack routing done.  testing components

--- client/src/components/LogInModal/LogInModal.style.ts
+++ client/src/components/LogInModal/LogInModal.style.ts
@@ -1,7 +1,21 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface IProps {
+  triggerAnimation: boolean;
+}
 
 export const StyledSection = styled.section`
+  display: none;
   background: orangered;
+  transition: all 0.75s;
+  transform: translateY(-100%);
+
+  ${(props: IProps) =>
+    props.triggerAnimation &&
+    css`
+      ${({ theme: { flexin } }: any) => flexin('center', 'center', 'column')}
+      transform: translateY(0);
+    `}
 `;
 
 export const StyledH1 = styled.h1`
--- client/src/components/OtherUsers/OtherUsers.style.ts
+++ client/src/components/OtherUsers/OtherUsers.style.ts
@@ -1,16 +1,37 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 interface IProps {
   triggerAnimation: boolean;
 }
 
+const animateIn = keyframes`
+  0% {
+    transform: translateY(100%);
+  opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+  opacity: 1;
+  }
+`;
+
 export const StyledSection = styled.section`
   display: none;
   background: yellow;
+  transform: translateY(100%);
+  opacity: 0;
+  animation-duration: all 0.75s;
 
   ${(props: IProps) =>
     props.triggerAnimation &&
     css`
-      display: block;
+      ${({ theme: { flexin } }: any) => flexin('center', 'center', 'column')};
+      animation: ${animateIn} forwards;
+    `}
+
+  ${(props: IProps) =>
+    !props.triggerAnimation &&
+    css`
+      animation: ${animateIn} backwards;
     `}
 `;
