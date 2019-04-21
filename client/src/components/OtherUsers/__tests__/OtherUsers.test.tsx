c649a69953a8c4a698d1272857bebb93e116552a
Patrick <patrick.a.mclennan@gmail.com>
2019-04-20 21:41:31 -0400

    most functionality + full stack routing done.  testing components

--- client/src/components/OtherUsers/__tests__/OtherUsers.test.tsx
+++ client/src/components/OtherUsers/__tests__/OtherUsers.test.tsx
@@ -14,7 +14,7 @@ afterEach(cleanup);
 
 const changeComponent: Function = jest.fn();
 const changCurrentUser: Function = jest.fn();
-const otherUsers: IUser[] = [fakeUserSolo, fakeUserSolo, fakeUserSolo];
+const fakeOtherUsers: IUser[] = [fakeUserSolo, fakeUserSolo, fakeUserSolo];
 
 const renderOtherUsers = () =>
   render(
@@ -22,18 +22,34 @@ const renderOtherUsers = () =>
       <OtherUsers
         changeComponent={changeComponent}
         changeCurrentUser={changCurrentUser}
-        otherUsers={otherUsers}
+        otherUsers={fakeOtherUsers}
       />
     </ThemeProvider>
   );
 
 test('<OtherUsers />', () => {
-  const { getByTestId, queryAllByTestId, debug } = renderOtherUsers();
+  const { getByTestId, queryAllByTestId } = renderOtherUsers();
   const otherUsers = getByTestId('otherUsers');
   const badges = queryAllByTestId('badge');
+  const badgeNames = queryAllByTestId('badge__name');
+  const badgeHouse = queryAllByTestId('badge__house');
+  const badgeScore = queryAllByTestId('badge__score');
 
   // Content
   expect(otherUsers).toBeInTheDocument();
+  badges.forEach((userBadge: any) => {
+    expect(otherUsers).toContainElement(userBadge);
+  });
+  badgeNames.forEach((badgeName: any) => {
+    expect(badgeName.textContent).toBe(fakeUserSolo.name);
+  });
+  badgeHouse.forEach((badgeHouse: any) => {
+    expect(badgeHouse.textContent).toBe(`House ${fakeUserSolo.house.name}`);
+  });
+  badgeScore.forEach((badgeScore: any) => {
+    expect(badgeScore.textContent).toBe(`${fakeUserSolo.currentScore}`);
+  });
+  expect(badges.length).toBe(fakeOtherUsers.length);
 
   // Styles
   expect(otherUsers).toHaveStyleRule('background', 'yellow');
